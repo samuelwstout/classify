4d9c19bf6dd62888ea736696bb7c26105eae508c
samuelwstout <samuelwstout@gmail.com>
2022-11-21 13:33:38 -0600

    fix stack order bug

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,19 +2,18 @@ import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Button, CssBaseline, Box, Typography, Container } from '@mui/material'
 
-const AUTH_URL = 'https://accounts.spotify.com/authorize?client_id=a45eb12484d24c4199050bdefee6d24b&response_type=code&redirect_uri=https://classify.up.railway.app/&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state'
+const AUTH_URL = 'https://accounts.spotify.com/authorize?client_id=a45eb12484d24c4199050bdefee6d24b&response_type=code&redirect_uri=http://localhost:3000/&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state'
 
 const Login = ({ code }) => {
 
+    const navigate = useNavigate()
+
     useEffect(() => {
         if (code) {
-          navigate('../search')
+          navigate('/search')
         }
-        }, [])
-
-    const navigate = useNavigate()
+        }, [code])
     
-
     return (
         <Container component="main" maxWidth="xs">
             <CssBaseline />
