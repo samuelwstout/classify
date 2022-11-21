870ee98e79cc9d2d9cab21f810a9fd576482290c
samuelwstout <samuelwstout@gmail.com>
2022-11-21 10:18:17 -0600

    install react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,33 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
+import Login from './components/Login'
+import Search from './components/Search'
+import Results from './components/Results'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-function App() {
-  const [count, setCount] = useState(0)
+const code = new URLSearchParams(window.location.search).get('code')
+
+const App = () => {
+
+  const [composers, setComposers] = useState([])
+
+  useEffect(() => {
+    const fetchComposers = async () => {
+      const response = await fetch('composers.json')
+      const data = await response.json()
+      const listofComposers = data.composers.map((composer) => composer)
+      setComposers(listofComposers)
+    }
+    fetchComposers()
+  }, [])
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <Router>
+        <Routes>
+          <Route path='/' element={<Login code={code} />} />
+          <Route path='/search' element={<Search composerData={composers} />} />
+          <Route path='/results' element={<Results code={code} />} />
+        </Routes>
+    </Router>
   )
 }
 
