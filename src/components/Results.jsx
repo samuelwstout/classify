2cb0155ab5976146f115e64f05d716dab8c03283
samuelwstout <samuelwstout@gmail.com>
2022-11-22 15:48:01 -0600

    remove verbose fetch lines

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,31 +1,24 @@
 import { useState, useEffect } from 'react'
-import { selectName } from '../redux/composerSlice'
-import { useSelector } from 'react-redux'
 
-const Results = ({ accessToken, spotifyApi }) => {
+const Results = ({ spotifyApi }) => {
+
+  const accessToken = localStorage.accessToken
+  const composer = localStorage.composer
 
   const [artistId, setArtistId] = useState('')
 
-  const composer = useSelector(selectName)
-
   useEffect(() => {
-    if (!accessToken) return
     spotifyApi.setAccessToken(accessToken)
   }, [accessToken])
 
   useEffect(() => {
-    if (!composer) return setArtistId([])
-    if (!accessToken) return
-    const cancel = false
     spotifyApi.searchArtists(composer).then(res => {
-      if (cancel) return
       setArtistId(res.body.artists.items[0].id)
     })
-  }, [composer, accessToken])
-
+  }, [composer])
 
   return (
-    <> 
+    <>
       <h1>{composer}</h1>
       <h3>{artistId}</h3>
     </>
