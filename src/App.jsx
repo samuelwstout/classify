4894264e1d2ef3c5d771af71be3d50cc06816799
samuelwstout <samuelwstout@gmail.com>
2022-11-22 16:10:27 -0600

    integrate axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import Login from './components/Login'
 import Search from './components/Search'
 import Results from './components/Results'
@@ -12,27 +12,16 @@ const App = () => {
 
   const [composers, setComposers] = useState([])
 
-  useEffect(() => {
-    const fetchComposers = async () => {
-      const response = await fetch('https://api.openopus.org/composer/list/rec.json')
-      const data = await response.json()
-      const listofComposers = data.composers.map((composer) => composer)
-      setComposers(listofComposers)
-    }
-    fetchComposers()
-  }, [])
+  axios.get('https://api.openopus.org/composer/list/rec.json')
+    .then((r) => setComposers(r.data.composers.map(c => c)))
 
-  useEffect(() => {
-    if (code) {
-    axios.post('http://localhost:3001/login', {
-        code,
-    })
+  if (code) {
+    axios.post('http://localhost:3001/login', code)
     .then(res => {
       localStorage.setItem('accessToken', res.data.accessToken)
       localStorage.setItem('code', code)
     })
-    }
-   }, [code])
+  }
 
   const spotifyApi = new SpotifyWebApi({
     clientId: 'a45eb12484d24c4199050bdefee6d24b',
