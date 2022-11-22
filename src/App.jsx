6d321fbb8e73804b44d9aeda1b07f8394a74510d
samuelwstout <samuelwstout@gmail.com>
2022-11-22 15:45:09 -0600

    post to backend in app.js

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import Search from './components/Search'
 import Results from './components/Results'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import SpotifyWebApi from 'spotify-web-api-node'
-import useAuth from './components/useAuth'
+import axios from 'axios'
 
-const code = new URLSearchParams(window.location.search).get('code')
+let code = new URLSearchParams(window.location.search).get('code')
 
 const App = () => {
 
@@ -22,21 +22,25 @@ const App = () => {
     fetchComposers()
   }, [])
 
+  useEffect(() => {
+    if (code) {
+    axios.post('http://localhost:3001/login', {
+        code,
+    })
+    .then(res => localStorage.setItem('accessToken', res.data.accessToken))
+    }
+   }, [code])
+
   const spotifyApi = new SpotifyWebApi({
     clientId: 'a45eb12484d24c4199050bdefee6d24b',
   })
-
-  let accessToken
-  if (code) {
-  accessToken = useAuth(code)
-  }
-
+  
   return (
     <Router>
         <Routes>
           <Route path='/' element={<Login code={code} />} />
           <Route path='/search' element={<Search composers={composers} />} />
-          <Route path='/results' element={<Results accessToken={accessToken} spotifyApi={spotifyApi} />} />
+          <Route path='/results' element={<Results spotifyApi={spotifyApi} />} />
         </Routes>
     </Router>
   )
