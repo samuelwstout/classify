104c06c04317a29cd3e3031da847c0f99b47ba96
samuelwstout <samuelwstout@gmail.com>
2022-11-25 00:08:54 -0600

    return to original api call syntax

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,21 +1,32 @@
 import { useState, useEffect } from 'react'
+import SpotifyWebApi from 'spotify-web-api-node'
+import useAuth from './useAuth'
 
-const Results = ({ spotifyApi }) => {
+const spotifyApi = new SpotifyWebApi({
+  clientId: 'a45eb12484d24c4199050bdefee6d24b',
+})
 
-  const accessToken = localStorage.accessToken
-  const composer = localStorage.composer
+const Results = ({ code }) => {
 
   const [artistId, setArtistId] = useState('')
 
+  const accessToken = useAuth(code)
+  const composer = localStorage.composer
+
   useEffect(() => {
+    if (!accessToken) return
     spotifyApi.setAccessToken(accessToken)
   }, [accessToken])
 
   useEffect(() => {
+    if (!composer) return setArtistId([])
+    if (!accessToken) return
+    let cancel = false
     spotifyApi.searchArtists(composer).then(res => {
+      if (cancel) return
       setArtistId(res.body.artists.items[0].id)
     })
-  }, [composer])
+  }, [composer, accessToken])
 
   return (
     <>
