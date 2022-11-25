08e83a92a4db6d65ff3db080f794ac5ceacb862d
samuelwstout <samuelwstout@gmail.com>
2022-11-25 00:08:28 -0600

    remove localstorage code

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,13 +6,12 @@ const AUTH_URL_LOCAL = 'https://accounts.spotify.com/authorize?client_id=a45eb12
 
 const AUTH_URL_PROD = 'https://accounts.spotify.com/authorize?client_id=a45eb12484d24c4199050bdefee6d24b&response_type=code&redirect_uri=https://classify.up.railway.app/&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state'
 
-
-const Login = () => {
+const Login = ({ code }) => {
 
     const navigate = useNavigate()
 
     useEffect(() => {
-        if (localStorage.code) {
+        if (code) {
           navigate('/search')
         }
         }, [])
