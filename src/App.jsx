4c94d388c3c3ce38f7c25d09120cee5ec7749f90
samuelwstout <samuelwstout@gmail.com>
2022-11-25 18:53:05 -0600

    remove spotify api call in results

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,14 @@ import Search from './components/Search'
 import Results from './components/Results'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import axios from 'axios'
+import SpotifyWebApi from 'spotify-web-api-node'
+import useAuth from './components/useAuth'
 
 const code = new URLSearchParams(window.location.search).get('code')
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: 'a45eb12484d24c4199050bdefee6d24b',
+})
 
 const App = () => {
 
@@ -16,12 +22,24 @@ const App = () => {
     .then((r) => setComposers(r.data.composers.map(c => c)))
   }, [])
 
+  let accessToken;
+
+  if (code) {
+    accessToken = useAuth(code)
+  }
+
+  useEffect(() => {
+    if (!accessToken) return
+    spotifyApi.setAccessToken(accessToken)
+  }, [accessToken])
+
+
   return (
     <Router>
         <Routes>
           <Route path='/' element={<Login code={code} />} />
           <Route path='/search' element={<Search composers={composers} />} />
-          <Route path='/results' element={<Results code={code} />} />
+          <Route path='/results' element={<Results spotifyApi={spotifyApi} accessToken={accessToken} />} />
         </Routes>
     </Router>
   )
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,22 +1,10 @@
 import { useState, useEffect } from 'react'
-import SpotifyWebApi from 'spotify-web-api-node'
-import useAuth from './useAuth'
 
-const spotifyApi = new SpotifyWebApi({
-  clientId: 'a45eb12484d24c4199050bdefee6d24b',
-})
-
-const Results = ({ code }) => {
+const Results = ({ accessToken, spotifyApi }) => {
 
   const [artistId, setArtistId] = useState('')
 
-  const accessToken = useAuth(code)
   const composer = localStorage.composer
-
-  useEffect(() => {
-    if (!accessToken) return
-    spotifyApi.setAccessToken(accessToken)
-  }, [accessToken])
 
   useEffect(() => {
     if (!composer) return setArtistId([])
