f37251136c03a8c0779d257a81e7b602ce501240
samuelwstout <samuelwstout@gmail.com>
2022-11-26 00:10:22 -0600

    integrate player in tracks.jsx

--- src/components/Tracks.jsx
+++ src/components/Tracks.jsx
@@ -1,11 +1,15 @@
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import Player from './Player'
+import useToggle from './useToggle'
 
 const Tracks = ({ spotifyApi, accessToken, albumImg }) => {
 
     const params = useParams().id
 
     const [tracks, setTracks] = useState([])
+    const [value, toggleValue] = useToggle(false)
+    const [playingTrack, setPlayingTrack] = useState([])
 
     useEffect(() => {
         if (!params) return setTracks([])
@@ -15,19 +19,22 @@ const Tracks = ({ spotifyApi, accessToken, albumImg }) => {
           if (cancel) return
             const tracks = res.body.items.map(t => {
                 return (
-                    <div key={t.id}>
-                        <h5>{t.name}</h5>
-                    </div>
+                      <div key={t.id} onClick={() => setPlayingTrack(t)}>
+                        <div onClick={toggleValue}>
+                            <h5>{t.name}</h5>
+                        </div>
+                      </div>
                 )
             })
             setTracks(tracks)
         }) 
-      }, [params, accessToken])
+      }, [params, accessToken, value])
 
   return (
     <>
         <img src={albumImg}></img>
         {tracks}
+        <Player accessToken={accessToken} trackUri={value ? playingTrack.uri : null} />
     </>
   )
 }
