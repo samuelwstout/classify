9d38eabad2614f35a59b3a11e9f831fb591eb2a8
samuelwstout <samuelwstout@gmail.com>
2022-11-26 00:09:55 -0600

    build player

---
+++ src/components/Player.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react'
+import SpotifyPlayer from 'react-spotify-web-playback'
+
+const Player = ({ accessToken, trackUri }) => {
+
+    const [play, setPlay] = useState(false)
+
+    useEffect(() => setPlay(true), [trackUri])
+
+    useEffect(() => {
+        if (trackUri === null) {
+            setPlay(false)
+        }
+    }, [trackUri])
+
+    if (!accessToken) return null
+
+  return (
+    <SpotifyPlayer
+        token={accessToken}
+        showSaveIcon
+        callback={state => {
+            if (!state.isPlaying) setPlay(false)
+        }}
+        play={play}
+        uris={trackUri ? [trackUri] : []}
+    />
+  )
+}
+
+export default Player
