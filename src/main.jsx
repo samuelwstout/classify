440b8a5d9c580edc73f2d68d2d51b271369bacb9
samuelwstout <samuelwstout@gmail.com>
2022-11-28 10:14:37 -0600

    apply spotify logo

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,9 @@ import { createTheme, ThemeProvider } from '@mui/material/styles'
 const theme = createTheme({
   palette: {
     mode: 'dark',
+    primary: {
+      main: '#fff'
+    }
   },
 });
 
