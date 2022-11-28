8b5238023ac436ec48e9f7cf352735183d60ec50
samuelwstout <samuelwstout@gmail.com>
2022-11-28 09:44:48 -0600

    integrate mui theme

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#0a1b2f'
+    }
+  }
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </>
 )
